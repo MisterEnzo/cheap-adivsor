cb800d34f6f736a9d059b101bcd7685c38cf548b
Enrique Naguiat <enzo_naguiat@yahoo.com>
2017-10-09 13:01:48 +0900

    updates on cheap adviser

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'contact', to: "pages#contact"
+
+  get 'about', to: "pages#about"
+
+  get root to: "pages#home"
+
+  get 'restaurants', to: "restaurants#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
